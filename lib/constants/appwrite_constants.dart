e66e80479b5cb21bf5d148d0423a67d17958165c
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-17 20:17:54 +0530

    Uploading the project

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '643d58d062222848dd39';
+  static const String projectId = '643d4d2f4c63ead2957c';
+  static const String endPoint = 'http://localhost:80/v1';
+}
